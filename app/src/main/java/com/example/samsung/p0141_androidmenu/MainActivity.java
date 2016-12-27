998f5de655787761bca7b68f7164e413f042a691
btow <btow@yandex.ru>
2016-12-27 17:07:40 +0500

    Change visibility of menu items

--- app/src/main/java/com/example/samsung/p0141_androidmenu/MainActivity.java
+++ app/src/main/java/com/example/samsung/p0141_androidmenu/MainActivity.java
@@ -8,13 +8,16 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.CheckBox;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView tvOut;
     ImageView imageView;
+    CheckBox checkBox1, checkBox2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,13 +37,27 @@ public class MainActivity extends AppCompatActivity {
 
         tvOut = (TextView) findViewById(R.id.tvOut);
         imageView = (ImageView) findViewById(R.id.imageView);
+        checkBox1 = (CheckBox) findViewById(R.id.checkBox1);
+        checkBox2 = (CheckBox) findViewById(R.id.checkBox2);
+
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+
+        menu.setGroupVisible(R.id.group1, checkBox1.isChecked());
+        menu.setGroupVisible(R.id.group_mail, checkBox2.isChecked());
+        return super.onPrepareOptionsMenu(menu);
+
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
+
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
+
     }
 
     @Override
@@ -51,8 +68,27 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (id) {
+            case R.id.action_settings :
+                tvOut.setText(getString(R.string.hello_world));
+                imageView.setImageResource(R.mipmap.ic_launcher);
+                Toast.makeText(MainActivity.this, getString(R.string.action_settings), Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.action_item1 :
+                tvOut.setText(getString(R.string.action_item1));
+                imageView.setImageResource(R.drawable.img_20130703_185525);
+                Toast.makeText(MainActivity.this, getString(R.string.action_item1), Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.action_item2 :
+                tvOut.setText(getString(R.string.action_item2));
+                imageView.setImageResource(R.drawable.img_20150305_194201);
+                Toast.makeText(MainActivity.this, getString(R.string.action_item2), Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.action_item3 :
+                tvOut.setText(getString(R.string.action_item3));
+                imageView.setImageResource(R.mipmap.ic_launcher);
+                Toast.makeText(MainActivity.this, getString(R.string.action_item3), Toast.LENGTH_SHORT).show();
+                break;
         }
 
         return super.onOptionsItemSelected(item);
